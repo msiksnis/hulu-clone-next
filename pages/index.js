3959092790e539b71da01dd9c1682b8cd3ab5d30
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2021-04-23 10:41:45 +0200

    Finished the application clone

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -11,6 +14,23 @@ export default function Home() {
       </Head>
       <Header />
       <Navbar />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3/${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
